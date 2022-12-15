f2a32fcd2a60feea76f68d633c44430afcec941b
Chase <98079776+chasevriend@users.noreply.github.com>
2022-12-15 11:03:07 -0800

    Add files via upload

---
+++ Etch a Sketch/script.js
@@ -0,0 +1,21 @@
+//Create Board using divs
+function createGrid() {
+    let grid = document.querySelector(".grid");
+    let squares = document.querySelector("div");
+    grid.style.gridTemplateColumns = "repeat(16 , 1fr)";
+    grid.style.gridTemplateColumns ="repeat(16 , 1fr)";
+
+    for (let i = 0; i < 256; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover" , () => {
+            square.style.backgroundColor = "black";
+        });
+        square.style.backgroundColor = "blue";
+        grid.insertAdjacentElement("beforeend", square);
+    }
+}
+
+createGrid(16);
+//Create a reset button that refreshes the entire drawing 
+
+
